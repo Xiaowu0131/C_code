3fd9b1d3cd2bd28445f3e1a82df2dc6d9afaab5f
Xiaowu0131 <139211762+Xiaowu0131@users.noreply.github.com>
2023-07-21 09:20:56 +0800

    Add files via upload
    while循环

---
+++ test_3/test_3/test_3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/*int main()
+{
+	int i = 1;
+	while (i <= 10)//判断表达式
+	{
+		printf("%d", i);
+		i++;
+	}
+	return 0;
+}
+
+*/
+
+/*int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i == 5)
+			break;
+			//在while循环里，break用于永久的循环终止。（f10逐行测试发现当i==5时直接跳出while循环）
+			//如是continue，continue作用是跳过本次循环语句里continue后面的语句，从新回到while后语句的判断。此处如果是continue则打印1 2 3 4然后陷入死循环。（f10逐行运行可以了解）
+		printf("%d", i);
+		i++;
+	}
+	return 0;
+}
+*/
+
+
+//// EOF 文件结束标志
+////getchar 读取一个字符
+////putchar 输出一个字符
+//int main()
+//{
+//	int ch = getchar();
+//	//printf("%c\n", ch);
+//	putchar(ch);
+//	return 0;
+//}
+
